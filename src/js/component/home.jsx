5a7bfa34331e0d01bde98a12ef6b39dc7729544b
jeff-c44 <156033827+jeff-c44@users.noreply.github.com>
2024-07-09 18:51:39 +0000

    Final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const Ref = useRef(null);
+    const [timer, setTimer] = useState("00:00:00");
+
+    
+    const startTimer = () => {
+        const startTime = new Date().getTime(); 
+
+        
+        const id = setInterval(() => {
+            const currentTime = new Date().getTime();
+            const elapsedTime = currentTime - startTime;
+
+            
+            const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+            const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+
+            
+            setTimer(
+                (hours > 9 ? hours : "0" + hours) +
+                ":" +
+                (minutes > 9 ? minutes : "0" + minutes) +
+                ":" +
+                (seconds > 9 ? seconds : "0" + seconds)
+            );
+        }, 1000);
+
+        Ref.current = id; 
+    };
+
+    
+    const clearTimer = () => {
+        clearInterval(Ref.current);
+    };
+
+    
+    useEffect(() => {
+        startTimer();
+
+        return () => {
+            clearInterval(Ref.current);
+        };
+    }, []);
+
+    
+    const onClickReset = () => {
+        clearInterval(Ref.current); 
+        setTimer("00:00:00"); 
+        startTimer(); 
+    };
+
+    return (
+      <div className="timerContainer">
+        <i className='far fa-clock' id="clock" style={{fontSize:"100px"}}></i>
+        <div className="timer">
+            <h2>{timer}</h2>
+            <button onClick={onClickReset}>Reset</button>
+        </div>
+        </div>
+    );
 };
 
 export default Home;
